8d90722282915e0c60adbe8ba1616b0d1fee91ea
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-11-22 14:27:17 -0500

    add project images

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import './projects.css'
 import P1 from '../../assets/P1.jpeg'
 import P2 from '../../assets/P2.webp'
-import P3 from '../../assets/P3.jpeg'
-import P4 from '../../assets/P4.png'
+import P3 from '../../assets/P3.jpg'
+import P4 from '../../assets/P4.jpg'
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+import { FcInfo } from 'react-icons/fc'
+import { BsDot } from 'react-icons/bs'
 
 const Projects = () => {
+
   return (
     <section id='projects'>
       <h5>My work</h5>
@@ -17,6 +22,17 @@ const Projects = () => {
             <img src={P1} alt="Connect 4" className="project-img"/>
           </div>
           <h3>Connect 4</h3>
+          <div className="info">
+            <Tippy content={
+              <ul>
+                <li><BsDot/>Javascript</li>
+                <li><BsDot/>CSS</li>
+                <li><BsDot/>HTML</li>
+              </ul>
+            }>
+              <span><FcInfo className='info-icon'/></span>
+            </Tippy>
+          </div>
           <div className='project-btn'>
             <a 
               href="https://github.com/LedioDylgjeri/Connect-4"
@@ -41,6 +57,21 @@ const Projects = () => {
             <img src={P2} alt="Connect 4" className="project-img"/>
           </div>
           <h3>Fantasy Soccer</h3>
+          <div className="info">
+            <Tippy content={
+              <ul>
+                <li><BsDot/>MongoDB</li>
+                <li><BsDot/>Mongoose</li>
+                <li><BsDot/>Javascript</li>
+                <li><BsDot/>Express</li>
+                <li><BsDot/>EJS</li>
+                <li><BsDot/>CSS</li>
+                <li><BsDot/>HTML</li>
+              </ul>
+            }>
+              <span><FcInfo className='info-icon'/></span>
+            </Tippy>
+          </div>
           <div className='project-btn'>
             <a 
               href="https://github.com/LedioDylgjeri/fantasy-soccer-team"
@@ -65,6 +96,21 @@ const Projects = () => {
             <img src={P3} alt="Connect 4" className="project-img"/>
           </div>
           <h3>Food Runner</h3>
+          <div className="info">
+            <Tippy content={
+              <ul>
+                <li><BsDot/>MongoDB</li>
+                <li><BsDot/>Mongoose</li>
+                <li><BsDot/>Express</li>
+                <li><BsDot/>React</li>
+                <li><BsDot/>Bootstrap</li>
+                <li><BsDot/>CSS</li>
+                <li><BsDot/>HTML</li>
+              </ul>
+            }>
+              <span><FcInfo className='info-icon'/></span>
+            </Tippy>
+          </div>
           <div className='project-btn'>
             <a 
               href="https://github.com/LedioDylgjeri/Food-Runner-Front"
@@ -89,6 +135,20 @@ const Projects = () => {
             <img src={P4} alt="Connect 4" className="project-img"/>
           </div>
           <h3>Car Collector</h3>
+          <div className='info'>
+            <Tippy content={
+              <ul>
+                <li><BsDot/>PostgreSQL</li>
+                <li><BsDot/>Python</li>
+                <li><BsDot/>Django</li>
+                <li><BsDot/>Docker</li>
+                <li><BsDot/>CSS</li>
+                <li><BsDot/>HTML</li>
+              </ul>
+            }>
+              <span><FcInfo className='info-icon'/></span>
+            </Tippy>
+          </div>
             <div className='project-btn'>
             <a 
               href="https://github.com/LedioDylgjeri/car-collector"
