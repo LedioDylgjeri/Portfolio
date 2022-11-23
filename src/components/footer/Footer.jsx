457265735cd30350a14e19263b296fdea9aa3b4f
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2022-11-23 15:34:32 -0500

    adjust footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,12 +8,6 @@ function Footer() {
       <a href="#landing" >
         <img src={Logo} alt="my logo" className='footer_logo'/>
       </a>
-      <ul className='links'>
-        <li><a href="#landing">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
       <div className="footer_copyright">
         <small>&copy; 2022 LEDIO DYLGJERI. ALL RIGHTS RESERVED</small>
       </div>
